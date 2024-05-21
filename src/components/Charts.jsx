e9339ef76f705459a17d9a78665fdc58230304ec
hakanerol <hakan-erol@outlook.com>
2024-05-22 00:11:41 +0200

    home updated

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,9 +1,49 @@
+import Grid from "@mui/material/Grid";
+import { AreaChart } from "@tremor/react";
+import { useSelector } from "react-redux";
 
+const dataFormatter = (number) =>
+  `€${Intl.NumberFormat("us").format(number).toString()}`;
 
-const Charts = () => {
+export default function Charts() {
+  const { sales, purchases } = useSelector((state) => state.stock);
+
+  const salesData = sales.map((item) => ({
+    date: new Date(item.updatedAt).toLocaleDateString("no-NO"),
+    amount: item.amount,
+  }));
+  const purchasesData = purchases.map((item) => ({
+    date: new Date(item.updatedAt).toLocaleDateString("no-NO"),
+    amount: item.amount,
+  }));
+
   return (
-    <div>Charts</div>
-  )
-}
+    <Grid container spacing={2} marginTop={2} justifyContent={"center"}>
+      <Grid item md={10} lg={6} minWidth={350}>
+        <AreaChart
+          className="h-80"
+          data={salesData}
+          index="date"
+          categories={["amount"]}
+          colors={["indigo"]}
+          valueFormatter={dataFormatter}
+          yAxisWidth={60}
+          onValueChange={(v) => console.log(v)}
+        />
+      </Grid>
 
-export default Charts
+      <Grid item md={10} lg={6} minWidth={350}>
+        <AreaChart
+          className="h-80"
+          data={purchasesData}
+          index="date"
+          categories={["amount"]}
+          colors={["rose"]}
+          valueFormatter={dataFormatter}
+          yAxisWidth={60}
+          onValueChange={(v) => console.log(v)}
+        />
+      </Grid>
+    </Grid>
+  );
+}
